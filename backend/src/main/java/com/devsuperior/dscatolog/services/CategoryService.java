99cc579387bb54de1512949dc6f1fb27b349fb38
Junior Braguim <junior.braguim@outlook.com>
2021-12-16 07:07:33 -0300

    DTO object

--- backend/src/main/java/com/devsuperior/dscatolog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatolog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dscatolog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dscatolog.dto.CategoryDTO;
 import com.devsuperior.dscatolog.entities.Category;
 import com.devsuperior.dscatolog.repositories.CategoryRepository;
 
@@ -14,8 +16,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
